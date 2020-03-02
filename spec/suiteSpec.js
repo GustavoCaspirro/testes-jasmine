a085214270c1b0158e8c8e9efd36bade12759db8
Gustavo Caspirro <gustavo.caspirro-ext@viavarejo.com.br>
2020-03-02 16:03:18 -0300

    first commit

---
+++ spec/suiteSpec.js
@@ -0,0 +1,5 @@
+describe("Operação de Adicação", () => {
+  it("Deve garantir que a soma entre os valores está correto", () => {
+    expect();
+  })
+});
---
+++ spec/testesSpec.js
@@ -0,0 +1,9 @@
+describe("Testes (specs)", () => {
+  it("Deve garantir que 'true' é 'true'", () => {
+
+  });
+
+  it("Deve garantir que 'true' não é false", () => {
+
+  })
+});
